e814e41a7483dad44db6f3888d5f3f7101d3233e
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-04-23 10:13:32 -0300

    Utilizando DTO na Service

--- src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -21,7 +21,7 @@ public class AdocaoController {
     @Transactional
     public ResponseEntity<String> solicitar(@RequestBody @Valid SolicitacaoAdocaoDto dto) {
         try {
-            this.adocaoService.solicitar(adocao);
+            this.adocaoService.solicitar(dto);
             return ResponseEntity.ok("Adoção solicitada com sucesso!");
         } catch (ValidacaoException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -32,14 +32,14 @@ public class AdocaoController {
     @PutMapping("/aprovar")
     @Transactional
     public ResponseEntity<String> aprovar(@RequestBody @Valid AprovacaoAdocaoDto dto) {
-        this.adocaoService.aprovar(adocao);
+        this.adocaoService.aprovar(dto);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/reprovar")
     @Transactional
     public ResponseEntity<String> reprovar(@RequestBody @Valid ReprovacaoAdocaoDto dto) {
-        this.adocaoService.reprovar(adocao);
+        this.adocaoService.reprovar(dto);
         return ResponseEntity.ok().build();
     }
 
